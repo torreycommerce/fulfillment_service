51af92bdb9744fc84636c359986c09edf5c72e5a
Brandon <brandon@tonzof.com>
2019-02-21 13:04:56 -0700

    Kicking Build

--- worker.php
+++ worker.php
@@ -1,20 +1,23 @@
 <?php
 error_reporting(E_ALL);
-ini_set('display_errors',1);
+ini_set('display_errors', 1);
 
 //  AcendaWorker\Base handles: gearman connection - loads of configs - init of logger
-require_once __DIR__."/../../core/Base.php";
+require_once __DIR__ . "/../../core/Base.php";
 require_once(__DIR__ . "/classes/FulfillmentService.php");
 require_once __DIR__ . "/vendors/autoload.php";
 
-class WorkerFulfillmentService extends AcendaWorker\Base {
+class WorkerFulfillmentService extends AcendaWorker\Base
+{
     private $FulfillmentService;
 
-    public function __construct() {
+    public function __construct()
+    {
         parent::__construct(__DIR__);
     }
 
-    public function FulfillmentService($job) {
+    public function FulfillmentService($job)
+    {
         $this->FulfillmentService = new FulfillmentService(array_merge_recursive($this->configs->service, $this->configs->job), $this->logger, $this->getCouchBase());
         // If you want to test - comment out this line (And comment out process)
         // Then set the string contents of the file in the service subscription
@@ -24,7 +27,7 @@ class WorkerFulfillmentService extends AcendaWorker\Base {
 }
 
 
-$workerFulfillmentService  = new WorkerFulfillmentService();
+$workerFulfillmentService = new WorkerFulfillmentService();
 $workerFulfillmentService->worker->addFunction('fulfillment',
-                                            [$workerFulfillmentService, 'FulfillmentService']);
+    [$workerFulfillmentService, 'FulfillmentService']);
 $workerFulfillmentService->worker->work();
